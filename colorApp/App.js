c5aef49fc11e0e113f79c218106f37e843990b71
xufeng1994 <914650562@qq.com>
2018-09-11 02:30:17 +0800

    修复一些bug leson 05-03

--- colorApp/App.js
+++ colorApp/App.js
@@ -6,103 +6,15 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet,ListView,AsyncStorage} from 'react-native';
-import ColorButton from './components/ColorButton'
-import ColorForm from './components/ColorForm'
+import React, { Component } from 'react';
+import {Alert } from 'react-native';
+import ColorList from './components/ColorList';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
 
-
-export default class App extends Component{
-
-  constructor(){
-    super();
-    this.ds = new ListView.DataSource({
-      rowHasChanged:(r1,r2) => r1 !== r2
-    })
-    const availableColors = []
-    this.state ={
-      backgroundColor:'blue',
-      availableColors,
-      dataSource:this.ds.cloneWithRows(availableColors)
-    }
-    this.changeColor = this.changeColor.bind(this)
-    this.newColor = this.newColor.bind(this)
-  }
-
-  componentDidMount(){
-    AsyncStorage.getItem(
-      '@ColorListStore:Colors',
-      (err,data)=>{
-        if(err){
-          console.error('Error Loading colors',err)
-        }else{
-          const availableColors = JSON.parse(data)
-          this.setState({
-            availableColors,
-            dataSource:this.ds.cloneWithRows(availableColors)
-          })
-          }
-        }
-      )
-    }
-  saveColor(colors){
-    AsyncStorage.setItem(
-      '@ColorListStore:Colors',
-      JSON.stringify(colors)
+  render() {
+    return(
+      <ColorList onColorSelected ={color=>Alert.alert(color)}/>
     )
   }
-
-  changeColor(backgroundColor){
-    this.setState({backgroundColor})
-  }
-
-  newColor(color){
-    const availableColors =[
-      ...this.state.availableColors,
-      color
-    ]
-    this.setState({
-      availableColors,
-      dataSource:this.ds.cloneWithRows(availableColors)
-    })
-    this.saveColor(availableColors)
-  }
- 
-  render() {
-    const {backgroundColor,dataSource} = this.state
-    return (
-      
-      <ListView style={[styles.container,{backgroundColor}]}
-        dataSource ={dataSource}
-        renderRow={(color)=>(
-          <ColorButton backgroundColor ={color}
-            onSelect = {this.changeColor}/>
-        )}
-        renderHeader = {()=>(
-          <ColorForm  onNewColor ={this.newColor}/>
-        )}>
-        
-      </ListView>
-      
-    );
-  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  header:{
-    backgroundColor:'lightgrey',
-    paddingTop:20,
-    fontSize:30,
-    textAlign:'center'
-  }
-});
---
+++ colorApp/components/ColorList.js
@@ -0,0 +1,107 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import { StyleSheet,ListView,AsyncStorage} from 'react-native';
+import ColorButton from './ColorButton'
+import ColorForm from './ColorForm'
+
+
+
+
+export default class ColorList extends Component{
+
+  constructor(){
+    super();
+    this.ds = new ListView.DataSource({
+      rowHasChanged:(r1,r2) => r1 !== r2
+    })
+    const availableColors = []
+    this.state ={
+      availableColors,
+      dataSource:this.ds.cloneWithRows(availableColors)
+    }
+    this.newColor = this.newColor.bind(this)
+  }
+
+  componentDidMount(){
+    AsyncStorage.getItem(
+      '@ColorListStore:Colors',
+      (err,data)=>{
+        if(err){
+          console.error('Error Loading colors',err)
+        }else{
+          const availableColors = JSON.parse(data)
+          this.setState({
+            availableColors,
+            dataSource:this.ds.cloneWithRows(availableColors)
+          })
+          }
+        }
+      )
+    }
+  saveColor(colors){
+    AsyncStorage.setItem(
+      '@ColorListStore:Colors',
+      JSON.stringify(colors)
+    )
+  }
+
+
+
+  newColor(color){
+    const availableColors =[
+      ...this.state.availableColors,
+      color
+    ]
+    this.setState({
+      availableColors,
+      dataSource:this.ds.cloneWithRows(availableColors)
+    })
+    this.saveColor(availableColors)
+  }
+ 
+  render() {
+    const {backgroundColor,dataSource} = this.state
+    return (
+      
+      <ListView style={[styles.container,{backgroundColor}]}
+        dataSource ={dataSource}
+        renderRow={(color)=>(
+          <ColorButton backgroundColor ={color}
+            onSelect = {this.props.onColorSelected}/>
+        )}
+        renderHeader = {()=>(
+          <ColorForm  onNewColor ={this.newColor}/>
+        )}>
+        
+      </ListView>
+      
+    );
+  }
+}
+
+ColorList.defaultProps = {
+    onColorSelected: f=>f
+}
+
+ColorList.propTypes ={
+    //onColorSelected:React.propTypes.func
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header:{
+    backgroundColor:'lightgrey',
+    paddingTop:20,
+    fontSize:30,
+    textAlign:'center'
+  }
+});
